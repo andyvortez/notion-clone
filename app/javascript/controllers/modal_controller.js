12fed9ecf0653d7a9f0da37c689a65baac0025e8
Andy Velasquez <andyvelasquez@Andys-MacBook-Air.local>
2025-07-20 22:57:10 -0500

    Add log-out modal

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  static targets = ["content"]
+  connect() {
+    this.boundCloseOnOutsideClick = this.closeOnOutsideClick.bind(this)
+    document.addEventListener('click', this.boundCloseOnOutsideClick)
+  }
+
+  disconnect() {
+    document.removeEventListener('click', this.boundCloseOnOutsideClick)
+  }
+
+  open() {
+    this.contentTarget.style.display = "block"
+    console.log("click")
+  }
+
+  close() {
+    this.contentTarget.style.display = "none"
+  }
+
+  closeOnOutsideClick(event) {
+    if (this.contentTarget.style.display !== "none") {
+      // Don't close if clicking on the modal OR the trigger button
+      if (!this.contentTarget.contains(event.target) && 
+          !this.element.querySelector('.account-modal').contains(event.target)) {
+        this.close()
+      }
+    }
+  }
+}
